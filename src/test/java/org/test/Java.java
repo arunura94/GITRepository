ad41e6c2fb0e857830eed477fe92f52a26bfe85b
arunura94 <arunkamalesh94@gmail.com>
2024-09-12 11:23:32 +0530

    done

---
+++ src/test/java/org/test/Java.java
@@ -0,0 +1,15 @@
+package org.test;
+
+public class Java {
+	
+	private void empName() {
+		System.out.println("Arun");
+
+	}
+	public static void main(String[] args) {
+		Java j=new Java();
+		
+		j.empName();
+	}
+
+}
